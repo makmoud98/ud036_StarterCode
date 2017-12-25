74a12738e9b0d22ef510f90591f07494f4f952dc
makmoud98 <makmoud98@gmail.com>
2017-12-24 22:19:56 -0600

    changed to pass pep8 guidelines

--- media.py
+++ media.py
@@ -1,22 +1,21 @@
-#media.py
-
 class Movie():
-	#initialize the movie object with the required content
-	def __init__(self, title, desc, rating, p_url, yt_url):
-		self.title = title
-		self.description = desc 
-		self.poster_image_url = p_url
-		self.trailer_youtube_url = yt_url
-		self.set_valid_rating(rating)
+    # init the movie object(self, string, string, string, string, string)
+    def __init__(self, title, desc, rating, p_url, yt_url):
+        self.title = title
+        self.description = desc
+        self.poster_image_url = p_url
+        self.trailer_youtube_url = yt_url
+        self.set_valid_rating(rating)
 
-	#this function ensures a valid rating is entered, also, empty
-	def set_valid_rating(self, rating):
-		try: 
-			self.get_valid_ratings().index(rating)#this will throw an exception if rating is not in the list of valid ratings
-			self.rating = rating#sets the valid rating 
-		except ValueError:# either rating is not specified or the rating is not valid
-			print("No valid rating was entered for " + title);
-			self.rating = "NR"# NR for Not-Rated
+    # this function ensures a valid rating is entered, also, empty
+    def set_valid_rating(self, rating):
+        try:
+        	# throws an exception if rating is not valid
+            self.get_valid_ratings().index(rating)
+            self.rating = rating  # sets the valid rating
+        except ValueError:  # rating is not specified or the rating isn't valid
+            print("No valid rating was entered for " + title)
+            self.rating = "NR"  # NR for Not-Rated
 
-	def get_valid_ratings(self):
-		return ["G", "PG", "PG-13", "R"]
+    def get_valid_ratings(self):
+        return ["G", "PG", "PG-13", "R"]
